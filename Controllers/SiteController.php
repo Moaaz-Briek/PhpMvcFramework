ed5240b27b1c09ab48fe1242de515ab24f4cfa8e
moaaz97 <moaaz.briek97@gmail.com>
2023-02-12 23:47:27 +0200

    Render Contact form from the SiteController

--- Controllers/SiteController.php
+++ Controllers/SiteController.php
@@ -1,8 +1,14 @@
 <?php
 namespace app\Controllers;
 
+use app\Core\Application;
+
 class SiteController
 {
+    public static function contact()
+    {
+        return Application::$app->router->renderView('contact');
+    }
     public static function handleContact()
     {
         return 'Handling';
--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ use app\Core\Application;
 
 $app = new Application(dirname(__DIR__));
 $app->router->get('/', 'home');
-$app->router->get('/contact', 'contact');
+$app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
 $app->run();
