ebef7ca5ec642a3de8cb81b2ff792909fb38bf20
moaaz97 <moaaz.briek97@gmail.com>
2023-02-12 23:35:49 +0200

    Add Container class to the main layout and removing it from the contact form

--- Views/contact.php
+++ Views/contact.php
@@ -1,24 +1,22 @@
-<div class="container ">
-    <h5>Contact Us</h5>
-    <div class="mb-3 row">
-        <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Subject</label>
-            <input type="text"  class="form-control" name="subject">
-        </div>
-        <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-            <input type="text"  class="form-control" name="email">
-        </div>
-        <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Body</label>
-            <input type="text"  class="form-control" name="body">
-        </div>
-        <div class="col-sm-10">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <div class="col-sm-10 mt-5">
-            <input type="submit" class="form-control btn btn-primary" value="Send">
-        </div>
+<h5>Contact Us</h5>
+<div class="mb-3 row">
+    <div class="col-sm-10">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Subject</label>
+        <input type="text"  class="form-control" name="subject">
+    </div>
+    <div class="col-sm-10">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <input type="text"  class="form-control" name="email">
+    </div>
+    <div class="col-sm-10">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Body</label>
+        <input type="text"  class="form-control" name="body">
+    </div>
+    <div class="col-sm-10">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="col-sm-10 mt-5">
+        <input type="submit" class="form-control btn btn-primary" value="Send">
     </div>
 </div>
