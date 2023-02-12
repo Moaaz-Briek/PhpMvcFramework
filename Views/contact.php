b30a2e81e62533b5208bd3fc0676be31fc37a86c
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 00:46:20 +0200

    Remove unneeded for attribute

--- Views/contact.php
+++ Views/contact.php
@@ -2,19 +2,19 @@
 <form action="" method="post">
     <div class="mb-3 row">
         <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Subject</label>
+        <label class="col-sm-2 col-form-label">Subject</label>
             <input type="text"  class="form-control" name="subject">
         </div>
         <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <label class="col-sm-2 col-form-label">Email</label>
             <input type="text"  class="form-control" name="email">
         </div>
         <div class="col-sm-10">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Body</label>
+        <label class="col-sm-2 col-form-label">Body</label>
             <input type="text"  class="form-control" name="body">
         </div>
         <div class="col-sm-10">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+        <label class="col-sm-2 col-form-label">Password</label>
             <input type="password" class="form-control" name="password">
         </div>
         <div class="col-sm-10 mt-5">
