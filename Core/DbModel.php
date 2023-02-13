51d332dcb66e7fba28cb20dafd602586ef8d747d
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 23:55:21 +0200

    Save data to it's table Method

--- Core/DbModel.php
+++ Core/DbModel.php
@@ -6,8 +6,18 @@ abstract class DbModel extends Model
 {
     public abstract function tableName(): string;
 
+    public abstract function attributes(): array;
+
     public function save()
     {
+        $tableName = $this->tableName();
+        $attributes = $this->attributes();
+        $params = array_map(fn($attr) => ':$attr', $attributes);
+        $statement = self::prepare("INSERT INTO $tableName (".implode(',', $attributes).") VALUES (".implode(',', $params).")");
+    }
 
+    public static function prepare($query)
+    {
+        Application::$app->db->PDO->prepare($query);
     }
 }
