043ac6debc6f84f1927721f6a35e57dd89bdd1e1
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 22:58:22 +0200

    Apply, Save migration methods

--- Core/Database.php
+++ Core/Database.php
@@ -13,6 +13,31 @@ class Database
         $this->PDO = new \PDO($dsn, $user, $password);
         $this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigration();
+        $newMigrations = [];
+        $files = scandir(Application::$ROOT_DIR.'/migrations');
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+        foreach ($toApplyMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+            require_once Application::$ROOT_DIR.'/migrations/'.$migration;
+            $className = pathinfo($migration, PATHINFO_FILENAME); //Without the extension
+            $instance = new $className();
+            $this->log("Applying migration $migration");
+            $instance->up();
+            $this->log("Applied migration $migration");
+            $newMigrations[] = $migration;
+        }
+        if (!empty($newMigrations)) {
+            $this->saveMigrations($newMigrations);
+        } else {
+            $this->log("All migrations are applied.");
+        }
+    }
 
     public function createMigrationsTable()
     {
@@ -20,5 +45,24 @@ class Database
             id INT AUTO_INCREMENT PRIMARY KEY,
             migration VARCHAR(255),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigration()
+    {
+        $statement = $this->PDO->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
+
+    public function saveMigrations(array $migrations)
+    {
+        $migrationsAsStr = implode(",", array_map(fn($m) => "('$m')", $migrations));
+        $statement = $this->PDO->prepare("INSERT INTO migrations (migration) VALUES  $migrationsAsStr ");
+        $statement->execute();
+    }
+
+    protected function log($message)
+    {
+        echo '['.date('Y-m-d H:i:s').'] - ' . $message . PHP_EOL;
     }
 }
