5941c05e08a8a887fd93b3fee37471c8137e560d
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 23:08:29 +0200

    Edit User table query.

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -6,9 +6,9 @@ class m0001_initial {
         $db = \app\Core\Application::$app->db;
         $SQL = "CREATE TABLE users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                email VARCHA(255) NOT NULL,
-                firstname VARCHA(255) NOT NULL,
-                lastname VARCHA(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                firstname VARCHAR(255) NOT NULL,
+                lastname VARCHAR(255) NOT NULL,
                 status TINYINT NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 ) ENGINE=INNODB;";
