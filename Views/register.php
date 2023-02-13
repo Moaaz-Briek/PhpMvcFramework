f2ce968266d6e7a7b0025712a857cf651c436f97
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 17:39:27 +0200

    Editing the register form based on Form, Field classes

--- Views/register.php
+++ Views/register.php
@@ -1,37 +1,8 @@
-<form action="" method="post">
-<!--    <div class="row">-->
-<!--        <div class="col">-->
-<!--            <label class="col-sm-2 col-form-label">First Name</label>-->
-<!--            <input type="text"  class="form-control" name="firstname">-->
-<!--        </div>-->
-<!--        <div class="col">-->
-<!--            <label class="col-sm-2 col-form-label">Last Name</label>-->
-<!--            <input type="text"  class="form-control" name="lastname">-->
-<!--        </div>-->
-<!--    </div>-->
-    <div class="mb-3 row">
-        <div class="col-sm-10">
-        <label class="col-sm-2 col-form-label">First Name</label>
-            <input type="text"  class="form-control" name="firstname">
-        </div>
-        <div class="col-sm-10">
-        <label class="col-sm-2 col-form-label">Last Name</label>
-            <input type="text"  class="form-control" name="lastname">
-        </div>
-        <div class="col-sm-10">
-        <label class="col-sm-2 col-form-label">Email</label>
-            <input type="text"  class="form-control" name="email">
-        </div>
-        <div class="col-sm-10">
-        <label class="col-sm-2 col-form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <div class="col-sm-10">
-            <label class="col-sm-2 col-form-label">Confirm Password</label>
-            <input type="password" class="form-control" name="confirmPassword">
-        </div>
-        <div class="col-sm-10 mt-5">
-            <input type="submit" class="form-control btn btn-primary" value="Send">
-        </div>
-    </div>
-</form>
+<?php $form = \app\Core\form\Form::begin('', "post") ?>
+<?php echo $form->field($model, 'firstname'); ?>
+<?php echo $form->field($model, 'lastname'); ?>
+<?php echo $form->field($model, 'email'); ?>
+<?php echo $form->field($model, 'password')->passwordField(); ?>
+<?php echo $form->field($model, 'confirmPassword')->passwordField(); ?>
+<input type="submit" class="form-control btn btn-primary" value="Send">
+<?php \app\Core\form\Form::end()?>
