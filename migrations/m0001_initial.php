a0330525f196c0bf4fe0b4f0d0ebda66844651b2
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 23:06:27 +0200

    Create and Drop user table.

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -3,11 +3,22 @@
 class m0001_initial {
     public function up()
     {
-        echo 'Applying migration';
+        $db = \app\Core\Application::$app->db;
+        $SQL = "CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHA(255) NOT NULL,
+                firstname VARCHA(255) NOT NULL,
+                lastname VARCHA(255) NOT NULL,
+                status TINYINT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                ) ENGINE=INNODB;";
+        $db->PDO->exec($SQL);
     }
 
     public function down()
     {
-        echo 'Drop migration';
+        $db = \app\Core\Application::$app->db;
+        $SQL = "DROP TABLE users";
+        $db->PDO->exec($SQL);
     }
 }
