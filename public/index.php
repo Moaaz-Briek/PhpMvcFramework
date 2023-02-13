bb34f9d1af18a987e7d66ca2044fb6e387c4d8d6
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 21:59:22 +0200

    load the env file and passing the initialized config array to the app

--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
 
 use app\Controllers\AuthController;
 use app\Controllers\SiteController;
 use app\Core\Application;
 
-$app = new Application(dirname(__DIR__));
+require_once __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+  'db' => [
+    'dsn' => $_ENV['DB_DSN'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+  ],
+];
+
+$app = new Application(dirname(__DIR__), $config);
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
