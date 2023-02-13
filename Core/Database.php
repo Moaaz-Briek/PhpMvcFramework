60d40f3c4994985b3403cf6b4ae69e21206bb961
moaaz97 <moaaz.briek97@gmail.com>
2023-02-13 22:14:29 +0200

    migrationTable function

--- Core/Database.php
+++ Core/Database.php
@@ -13,4 +13,12 @@ class Database
         $this->PDO = new \PDO($dsn, $user, $password);
         $this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function createMigrationsTable()
+    {
+        $this->PDO->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=INNODB;");
+    }
 }
