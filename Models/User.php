521df93767c69553692506beefbb8f834d4c7ad4
moaaz97 <moaaz.briek97@gmail.com>
2023-02-14 07:18:46 +0200

    Define different status types

--- Models/User.php
+++ Models/User.php
@@ -6,9 +6,13 @@ use app\Core\DbModel;
 
 class User extends DbModel
 {
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_DELETED = 2;
     public string $firstname = '';
     public string $lastname = '';
     public string $email = '';
+    public int $status = self::STATUS_INACTIVE;
     public string $password = '';
     public string $confirmPassword = '';
 
@@ -32,11 +36,12 @@ class User extends DbModel
 
     public function attributes(): array
     {
-        return ['firstname', 'lastname', 'email', 'password'];
+        return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
 
     public function save()
     {
+        $this->status = self::STATUS_INACTIVE;
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return parent::save();
     }
