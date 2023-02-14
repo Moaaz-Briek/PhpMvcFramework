221762cf8a57544b47e495dde7d8d8b2990ebaf6
moaaz97 <moaaz.briek97@gmail.com>
2023-02-14 23:15:57 +0200

    Auth Login method

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -5,12 +5,22 @@ namespace app\Controllers;
 use app\Core\Application;
 use app\Core\Controller;
 use app\Core\Request;
+use app\Core\Response;
+use app\Models\LoginForm;
 use app\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request, Response $response)
     {
+        $loginForm = new LoginForm();
+        if ($request->isPost()) {
+            $loginForm->loadData($request->getBody());
+            if($loginForm->validate() && $loginForm->login()) {
+                $response->redirect('/');
+                return;
+            }
+        }
         $this->setLayout('auth');
         return $this->render('login');
     }
@@ -23,7 +33,7 @@ class AuthController extends Controller
             $user->loadData($request->getBody());
             if ($user->validate() && $user->save()) {
                 Application::$app->session->setFlash('success', 'Thanks for registering!');
-                Application::$app->request->redirect('/');
+                Application::$app->response->redirect('/');
             }
             return $this->render('register', [
                 'model' => $user
