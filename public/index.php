9b584801643416d30fec5927730e6458cb0c5432
moaaz97 <moaaz.briek97@gmail.com>
2023-02-15 00:33:31 +0200

    adding className in the config file, to make it more dynamically in related to the application, rather than use User class name, which may be changed in any time because it's not in the core.

--- public/index.php
+++ public/index.php
@@ -9,11 +9,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $config = [
-  'db' => [
-    'dsn' => $_ENV['DB_DSN'],
-    'user' => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASSWORD'],
-  ],
+    'userClass' => \app\Models\User::class,
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ],
 ];
 
 $app = new Application(dirname(__DIR__), $config);
