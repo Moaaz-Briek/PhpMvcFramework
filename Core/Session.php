0d6c1b9e08ecd8508ae2586f5fd1edb63bec3911
moaaz97 <moaaz.briek97@gmail.com>
2023-02-14 20:38:03 +0200

    Define our construct, destruct rules

--- Core/Session.php
+++ Core/Session.php
@@ -4,19 +4,30 @@ namespace app\Core;
 
 class Session
 {
+    protected const FLASH_KEY = 'flash_messages';
     public function __construct()
     {
         session_start();
+        $flashMessages = $_SESSION[self::FLASH_KEY];
+        foreach ($flashMessages as $key => $message) {
+            //Mark to be removed
+        }
     }
 
-    public function sotFlash($key, $message)
+    public function setFlash($key, $message)
     {
-
+        $_SESSION[self::FLASH_KEY][$key] = $message;
     }
 
     public function getFlash($key)
     {
 
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+        //Iterate over marked to
     }
 
 }
