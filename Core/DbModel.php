148b048fa339f45d8f1a07f0e03fc17757b20d47
moaaz97 <moaaz.briek97@gmail.com>
2023-02-15 00:02:10 +0200

    Finding record based on some attribute

--- Core/DbModel.php
+++ Core/DbModel.php
@@ -25,4 +25,19 @@ abstract class DbModel extends Model
     {
         return Application::$app->db->PDO->prepare($query);
     }
+
+    public function findOne(array $array)
+    {
+        $tableName = static::tableName();
+        $attribute = array_keys($array);
+        $sql = implode("AND ", array_map(fn($attr) => "$attr = :$attr", $attribute));
+        //SELECT * FROM $tableName WHERE email =:email AND firstname =:firstname
+        //SELECT * FROM $tableName WHERE $sql
+        $statement = self::prepare("SELECT * FROM $tableName WHERE $sql");
+        foreach ($array as $key => $value) {
+            $statement->bindValue(":$key", $value);
+        }
+        $statement->execute();
+        return $statement->fetchObject(static::class);
+    }
 }
