2716d24661ba2f265d98306c79ae7a89370db37d
moaaz97 <moaaz.briek97@gmail.com>
2023-02-16 08:25:58 +0200

    Apply middleware before access any action of AuthController.

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace app\Controllers;
 
 use app\Core\Application;
 use app\Core\Controller;
+use app\Core\middlewares\AuthMiddleware;
 use app\Core\Request;
 use app\Core\Response;
 use app\Models\LoginForm;
@@ -11,6 +12,11 @@ use app\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->registerMiddleware(new AuthMiddleware(['profile']));
+    }
+
     public function login(Request $request, Response $response)
     {
         $loginForm = new LoginForm();
