d62aff487fa472c3fa85fb794ef6cad67ed3b5c8
moaaz97 <moaaz.briek97@gmail.com>
2023-02-16 16:41:01 +0200

    Setting contact method of SiteController

--- Controllers/SiteController.php
+++ Controllers/SiteController.php
@@ -5,6 +5,8 @@ use app\Core\Application;
 use app\Core\Controller;
 use app\Core\middlewares\SiteMiddleware;
 use app\Core\Request;
+use app\Core\Response;
+use app\Models\ContactForm;
 
 class SiteController extends Controller
 {
@@ -15,22 +17,24 @@ class SiteController extends Controller
 
     public function home()
     {
-
         $params = [
-            'name' => 'Moaaz',
+            'name' => 'UserName',
         ];
         return $this->render('home', $params);
     }
 
-    public  function contact()
+    public  function contact(Request $request, Response $response)
     {
-        return $this->render('contact');
+        $contact = new ContactForm();
+        if ($request->isPost()) {
+            $contact->loadData($request->getBody());
+            if ($contact->validate() && $contact->send()) {
+                Application::$app->session->setFlash('success', 'Thanks For Contacting Us.');
+                return $response->redirect('/contact');
+            }
+        }
+        return $this->render('contact', [
+            'model' => $contact
+        ]);
     }
-
-    public function handleContact(Request $request)
-    {
-        $body = $request->getBody();
-        return 'Handling';
-    }
-
 }
